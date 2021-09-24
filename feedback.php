0ba3dc53e813c6a427a51c97c213d757d55f3ec4
Nikhil Prabhu K <74901434+prabhonik@users.noreply.github.com>
2021-09-24 06:22:21 +0530

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="feed.css">
-    <title>Maruthi Home</title>
+    <title>Feedback submission?</title>
 </head>
 <body>
     <header>
